02c6a0b4c64cba68b5d6ab820fda76079497e008
EliBui <91813317+EliBui@users.noreply.github.com>
2024-02-18 21:25:36 +0000

    done

--- src/hw1_main.c
+++ src/hw1_main.c
@@ -2,6 +2,64 @@
 #include "hw1.h"
 
 int main() {
+    // int array[] = 
+    // {17, 89, 42, 631, 52, 
+    // 77, 89, 100, 125, -6, 
+    // 823, 800, 1024, 1025, 9, 
+    // 1888, 0, -17, 19, 9999999, 
+    // -888888, 723, 1000, 1111, -99, 
+    // -95, 55};
+    // unsigned int array_len = 27;
+    // unsigned char* packets[4];
+    // unsigned int packets_len = 4;
+    // unsigned int max_payload = 20;
+    // unsigned int src_addr = 93737;
+    // unsigned int dest_addr = 10973;
+    // unsigned int src_port = 11;
+    // unsigned int dest_port = 6;
+    // unsigned int maximum_hop_count = 25;
+    // unsigned int compression_scheme = 3;
+    // unsigned int traffic_class = 14;
 
+    // int array[] = 
+    // {10, 11, 12, 13, 14,
+    //  15, 16, 17, 18, 19, 
+    //  20, 21};
+    // unsigned int array_len = 12;
+    // unsigned char* packets[3] = {0};
+    // unsigned int packets_len = 3;
+    // //unsigned int num_expected_packets = 3;
+    // unsigned int max_payload = 16;
+    // unsigned int src_addr = 123123;
+    // unsigned int dest_addr = 34534534;
+    // unsigned int src_port = 12;
+    // unsigned int dest_port = 13;
+    // unsigned int maximum_hop_count = 14;
+    // unsigned int compression_scheme = 0;
+    // unsigned int traffic_class = 15;
+
+    // int array[] = 
+    // {-6, 823, 9, 1888, 0,
+    //  -17, 9999999, -888888, 723, 1000,
+    //   1111};
+    // unsigned int array_len = 11;
+    // unsigned char* packets[4] = {0};
+    // unsigned int packets_len = 4;
+    // //unsigned int num_expected_packets = 4;
+    // unsigned int max_payload = 3 * sizeof(int);
+    // //printf("max payload: %u\n\n", max_payload);
+    // unsigned int src_addr = 93737;
+    // unsigned int dest_addr = 10973;
+    // unsigned int src_port = 11;
+    // unsigned int dest_port = 6;
+    // unsigned int maximum_hop_count = 25;
+    // unsigned int compression_scheme = 3;
+    // unsigned int traffic_class = 14;
+
+    // packetize_array_sf(array, array_len, packets, packets_len, max_payload, src_addr, dest_addr, src_port, dest_port, maximum_hop_count, compression_scheme, traffic_class);
+    // //unsigned int i = packetize_array_sf(array, array_len, packets, packets_len, max_payload, src_addr, dest_addr, src_port, dest_port, maximum_hop_count, compression_scheme, traffic_class);
+    //printf("%u\n", i);
     return 0;
 }
+
+
